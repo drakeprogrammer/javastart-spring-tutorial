c065bb8c02f749c80b0a9fc8747184b9bf13ccbd
drakeprogrammer <drakeprogrammers@gmail.com>
2019-02-10 19:48:09 +0100

    jpa-move-persistence-xml-configuration-to-java

--- src/main/java/pl/drakeprogrammer/config/JpaConfig.java
+++ src/main/java/pl/drakeprogrammer/config/JpaConfig.java
@@ -1,16 +1,41 @@
 package pl.drakeprogrammer.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class JpaConfig {
 
 	@Bean
-	public LocalEntityManagerFactoryBean createEMF() {
-		LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
+	public LocalContainerEntityManagerFactoryBean createEMF(JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		Map<String, String> properties = new HashMap<>();
+		properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/spring_framework");
+		properties.put("javax.persistence.jdbc.user", "root");
+		properties.put("javax.persistence.jdbc.password", "root");
+		properties.put("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
+		properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+
 		emf.setPersistenceUnitName("spring-jpa-pu");
+		emf.setJpaPropertyMap(properties);
+		emf.setJpaVendorAdapter(jpaVendorAdapter);
+		emf.setPackagesToScan("pl.drakeprogrammer.model");
+
 		return emf;
+	}
+
+	@Bean
+	public JpaVendorAdapter createJpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+		hibernateJpaVendorAdapter.setShowSql(true);
+		return hibernateJpaVendorAdapter;
 	}
 }
