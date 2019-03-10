c84c7c9d7b308998d121e2aa3de84adb2dcdc8f7
drakeprogrammer <drakeprogrammers@gmail.com>
2019-03-10 13:59:06 +0100

    spring-mvc-base-web-configuration-add-jpg-and-DefaultServletHandlerConfigurer-InternalResourceViewResolver

--- src/main/java/pl/drakeprogrammer/WebConfig.java
+++ src/main/java/pl/drakeprogrammer/WebConfig.java
@@ -1,12 +1,29 @@
 package pl.drakeprogrammer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
